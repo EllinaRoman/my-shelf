ebb889a7839c3c743837abe55726e8e7e1acd91d
EllinaRoman <evelinka.late@yandex.ru>
2026-04-26 22:24:14 +0300

    Исправлен путь к файлу sw

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ import { renderBooks } from './modules/library.js';
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(reg => {})
             .catch(err => console.error('Ошибка SW:', err));
     });
